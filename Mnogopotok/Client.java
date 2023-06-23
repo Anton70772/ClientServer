d24ab0f9c8c61fa9c026b6f307d0129305084358
Anton70772 <116624941+Anton70772@users.noreply.github.com>
2023-06-23 18:11:25 +0300

    server

---
+++ Mnogopotok/Client.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) {
+        try (Socket socket = new Socket("localhost", 8000)) {
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            Scanner scanner = new Scanner(System.in);
+            String message, response;
+            while (true) {
+                System.out.print("Enter message: ");
+                message = scanner.nextLine();
+                out.println(message);
+                if ("exit".equalsIgnoreCase(message)) {
+                    break;
+                }
+                response = in.readLine();
+                System.out.println("Response from server: " + response);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
