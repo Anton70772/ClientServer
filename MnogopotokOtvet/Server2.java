bac24e9101c302dee0af4d1fbe08f16caef4475b
Anton70772 <116624941+Anton70772@users.noreply.github.com>
2023-06-23 18:24:42 +0300

    mpotvet

---
+++ MnogopotokOtvet/Server2.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server2 {
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(8000)) {
+            System.out.println("Server started");
+            ExecutorService executor = Executors.newFixedThreadPool(10);
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                executor.submit(new ClientHandler(clientSocket));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static class ClientHandler implements Runnable {
+        private Socket socket;
+
+        public ClientHandler(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String message;
+                while ((message = in.readLine()) != null) {
+                    System.out.println("Received message from client: " + message);
+                    if ("exit".equalsIgnoreCase(message)) {
+                        break;
+                    }
+                    out.println("Server received message: " + message);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
